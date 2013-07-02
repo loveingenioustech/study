57bc08c507099e984420284d0c1bbcb202d333a2
Robin Long <loveingenioustech@gmail.com>
2013-07-02 15:42:25 +0800

    add java-demo project

---
+++ java-demo/src/demo/collection/RemoveDuplicatesFromArrayList.java
@@ -0,0 +1,37 @@
+package demo.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class RemoveDuplicatesFromArrayList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// ArrayList with duplicates String
+		List<String> duplicateList = (List<String>) Arrays.asList("Android",
+				"Android", "iOS", "Windows mobile");
+		// should print 4 becaues of duplicates Android
+		System.out.println("size of Arraylist with duplicates: "
+				+ duplicateList.size());
+		System.out.println("ArrayList with duplicates: " + duplicateList);
+
+		// Converting ArrayList to HashSet to remove duplicates
+		LinkedHashSet<String> listToSet = new LinkedHashSet<String>(
+				duplicateList);
+
+		// Creating Arraylist without duplicate values
+		List<String> listWithoutDuplicates = new ArrayList<String>(listToSet);
+		// should print 3 becaues of duplicates Android removed
+		System.out.println("size of ArrayList without duplicates: "
+				+ listToSet.size());
+		System.out
+				.println("ArrayList after removing duplicates in same order: "
+						+ listWithoutDuplicates);
+
+	}
+
+}
