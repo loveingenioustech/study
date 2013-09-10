a7ed6627b176b8beb2998abfb12ec6e0ac874329
Robin Long <loveingenioustech@gmail.com>
2013-09-10 09:35:27 +0800

    add table wd_user and test data

---
+++ web-demo/src/main/resources/db/tables.sql
@@ -0,0 +1,2 @@
+-- wd_user
+create table wd_user (username varchar2(20) not null, password varchar2(20), CONSTRAINT PK_USER PRIMARY KEY(username));
---
+++ web-demo/src/main/resources/db/testData.sql
@@ -0,0 +1,6 @@
+-- wd_user
+insert into wd_user values('robin','robin');
+insert into wd_user values('sean','sean');
+insert into wd_user values('roy','roy');
+
+commit;
