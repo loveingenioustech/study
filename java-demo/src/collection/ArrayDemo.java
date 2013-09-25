eb9ea86a7b176a64f459867247b2f7e2eb7b2c03
Robin Long <loveingenioustech@gmail.com>
2013-09-25 16:39:03 +0800

    update ArrayDemo add assemblingMultidimensionalArrays

--- java-demo/src/collection/ArrayDemo.java
+++ java-demo/src/collection/ArrayDemo.java
@@ -14,9 +14,11 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		ArrayDemo arrayDemo = new ArrayDemo();
 
-		arrayDemo.alphabeticSearch();
+//		arrayDemo.alphabeticSearch();
 
-		arrayDemo.searching();
+//		arrayDemo.searching();
+
+		arrayDemo.assemblingMultidimensionalArrays();
 	}
 
 	private void alphabeticSearch() {
@@ -37,7 +39,7 @@ public class ArrayDemo {
 		int[] a = ConvertTo.primitive(Generated.array(new Integer[25], gen));
 		Arrays.sort(a);
 		print("Sorted array: " + Arrays.toString(a));
-		
+
 		while (true) {
 			int r = gen.next();
 			int location = Arrays.binarySearch(a, r);
@@ -48,5 +50,17 @@ public class ArrayDemo {
 			}
 		}
 
+	}
+
+	private void assemblingMultidimensionalArrays() {
+		Integer[][] a;
+		a = new Integer[3][];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = new Integer[3];
+			for (int j = 0; j < a[i].length; j++)
+				a[i][j] = i * j; // Autoboxing
+		}
+
+		System.out.println(Arrays.deepToString(a));
 	}
 }
