4163923dec81287a77bfab034215bde8ae6f28c4
Robin Long <loveingenioustech@gmail.com>
2013-09-25 16:41:58 +0800

    update ArrayDemo add comparingArrays

--- java-demo/src/collection/ArrayDemo.java
+++ java-demo/src/collection/ArrayDemo.java
@@ -14,11 +14,13 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		ArrayDemo arrayDemo = new ArrayDemo();
 
-//		arrayDemo.alphabeticSearch();
+		// arrayDemo.alphabeticSearch();
 
-//		arrayDemo.searching();
+		// arrayDemo.searching();
 
-		arrayDemo.assemblingMultidimensionalArrays();
+		// arrayDemo.assemblingMultidimensionalArrays();
+
+		arrayDemo.comparingArrays();
 	}
 
 	private void alphabeticSearch() {
@@ -62,5 +64,25 @@ public class ArrayDemo {
 		}
 
 		System.out.println(Arrays.deepToString(a));
+	}
+
+	private void comparingArrays() {
+		int[] a1 = new int[10];
+		int[] a2 = new int[10];
+		
+		Arrays.fill(a1, 47);
+		Arrays.fill(a2, 47);
+		
+		print(Arrays.equals(a1, a2));
+		
+		a2[3] = 11;
+		print(Arrays.equals(a1, a2));
+		
+		String[] s1 = new String[4];
+		Arrays.fill(s1, "Hi");
+		String[] s2 = { new String("Hi"), new String("Hi"), new String("Hi"),
+				new String("Hi") };
+		
+		print(Arrays.equals(s1, s2));
 	}
 }
