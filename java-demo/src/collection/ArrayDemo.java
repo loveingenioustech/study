6c9aae2e28ca574d46a9b023f16704653d07aeba
Robin Long <loveingenioustech@gmail.com>
2013-09-25 16:45:52 +0800

    update ArrayDemo add copyingArrays

--- java-demo/src/collection/ArrayDemo.java
+++ java-demo/src/collection/ArrayDemo.java
@@ -20,7 +20,9 @@ public class ArrayDemo {
 
 		// arrayDemo.assemblingMultidimensionalArrays();
 
-		arrayDemo.comparingArrays();
+		// arrayDemo.comparingArrays();
+
+		arrayDemo.copyingArrays();
 	}
 
 	private void alphabeticSearch() {
@@ -69,20 +71,54 @@ public class ArrayDemo {
 	private void comparingArrays() {
 		int[] a1 = new int[10];
 		int[] a2 = new int[10];
-		
+
 		Arrays.fill(a1, 47);
 		Arrays.fill(a2, 47);
-		
+
 		print(Arrays.equals(a1, a2));
-		
+
 		a2[3] = 11;
 		print(Arrays.equals(a1, a2));
-		
+
 		String[] s1 = new String[4];
 		Arrays.fill(s1, "Hi");
 		String[] s2 = { new String("Hi"), new String("Hi"), new String("Hi"),
 				new String("Hi") };
-		
+
 		print(Arrays.equals(s1, s2));
 	}
+
+	private void copyingArrays() {
+		int[] i = new int[7];
+		int[] j = new int[10];
+		Arrays.fill(i, 47);
+		Arrays.fill(j, 99);
+
+		print("i = " + Arrays.toString(i));
+		print("j = " + Arrays.toString(j));
+		System.arraycopy(i, 0, j, 0, i.length);
+
+		print("j = " + Arrays.toString(j));
+
+		int[] k = new int[5];
+		Arrays.fill(k, 103);
+		System.arraycopy(i, 0, k, 0, k.length);
+		print("k = " + Arrays.toString(k));
+
+		Arrays.fill(k, 103);
+		System.arraycopy(k, 0, i, 0, k.length);
+		print("i = " + Arrays.toString(i));
+
+		// Objects:
+		Integer[] u = new Integer[10];
+		Integer[] v = new Integer[5];
+
+		Arrays.fill(u, new Integer(47));
+		Arrays.fill(v, new Integer(99));
+		print("u = " + Arrays.toString(u));
+		print("v = " + Arrays.toString(v));
+		System.arraycopy(v, 0, u, u.length / 2, v.length);
+		print("u = " + Arrays.toString(u));
+	}
+
 }
