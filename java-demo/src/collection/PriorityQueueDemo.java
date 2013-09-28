74ad6a9020c884c7453c99cf6d8615e6977c5c8d
Robin Long <loveingenioustech@gmail.com>
2013-09-28 21:19:57 +0800

    add QueueDemo and update PriorityQueueDemo

--- java-demo/src/collection/PriorityQueueDemo.java
+++ java-demo/src/collection/PriorityQueueDemo.java
@@ -7,6 +7,14 @@ import java.util.*;
  */
 public class PriorityQueueDemo {
 	public static void main(String[] args) {
+		PriorityQueueDemo demo = new PriorityQueueDemo();
+
+		// demo.test1();
+
+		demo.test2();
+	}
+
+	private void test1() {
 		PriorityQueue<GregorianCalendar> pq = new PriorityQueue<GregorianCalendar>();
 		pq.add(new GregorianCalendar(1906, Calendar.DECEMBER, 9)); // G. Hopper
 		pq.add(new GregorianCalendar(1815, Calendar.DECEMBER, 10)); // A.
@@ -24,4 +32,42 @@ public class PriorityQueueDemo {
 		while (!pq.isEmpty())
 			System.out.println(pq.remove().get(Calendar.YEAR));
 	}
+
+	private void test2() {
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>();
+		Random rand = new Random(47);
+		for (int i = 0; i < 10; i++)
+			priorityQueue.offer(rand.nextInt(i + 10));
+		QueueDemo.printQ(priorityQueue);
+
+		List<Integer> ints = Arrays.asList(25, 22, 20, 18, 14, 9, 3, 1, 1, 2,
+				3, 9, 14, 18, 21, 23, 25);
+		
+		priorityQueue = new PriorityQueue<Integer>(ints);
+		QueueDemo.printQ(priorityQueue);
+		
+		priorityQueue = new PriorityQueue<Integer>(ints.size(),
+				Collections.reverseOrder());
+		priorityQueue.addAll(ints);
+		
+		QueueDemo.printQ(priorityQueue);
+
+		String fact = "EDUCATION SHOULD ESCHEW OBFUSCATION";
+		List<String> strings = Arrays.asList(fact.split(""));
+		PriorityQueue<String> stringPQ = new PriorityQueue<String>(strings);
+		QueueDemo.printQ(stringPQ);
+		stringPQ = new PriorityQueue<String>(strings.size(),
+				Collections.reverseOrder());
+		stringPQ.addAll(strings);
+		QueueDemo.printQ(stringPQ);
+
+		Set<Character> charSet = new HashSet<Character>();
+		for (char c : fact.toCharArray())
+			charSet.add(c); // Autoboxing
+		PriorityQueue<Character> characterPQ = new PriorityQueue<Character>(
+				charSet);
+		QueueDemo.printQ(characterPQ);
+
+	}
+
 }
