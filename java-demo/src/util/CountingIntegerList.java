d288a67bc80a9728a8469ba61bb86428c60d6a7c
Robin Long <loveingenioustech@gmail.com>
2013-09-28 16:58:19 +0800

    add ListPerformanceDemo

---
+++ java-demo/src/util/CountingIntegerList.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.util.*;
+
+public class CountingIntegerList extends AbstractList<Integer> {
+	private int size;
+
+	public CountingIntegerList(int size) {
+		this.size = size < 0 ? 0 : size;
+	}
+
+	public Integer get(int index) {
+		return Integer.valueOf(index);
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new CountingIntegerList(30));
+	}
+}
