106ed5d6787a0707e65f64638182e1904ffeb65f
Robin Long <loveingenioustech@gmail.com>
2013-11-24 23:01:02 +1100

    update algorithm-demo add FrequencyCounter

--- algorithm-demo/src/searching/BinarySearch.java
+++ algorithm-demo/src/searching/BinarySearch.java
@@ -1,4 +1,3 @@
-package searching;
 /*************************************************************************
  *  Compilation:  javac BinarySearch.java
  *  Execution:    java BinarySearch whitelist.txt < input.txt
@@ -23,6 +22,8 @@ package searching;
  *  
  *************************************************************************/
 
+package searching;
+
 import java.util.Arrays;
 
 import stdlib.In;
@@ -31,30 +32,33 @@ import stdlib.StdOut;
 
 public class BinarySearch {
 
-    // precondition: array a[] is sorted
-    public static int rank(int key, int[] a) {
-        int lo = 0;
-        int hi = a.length - 1;
-        while (lo <= hi) {
-            // Key is in a[lo..hi] or not present.
-            int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
-            else return mid;
-        }
-        return -1;
-    }
+	// precondition: array a[] is sorted
+	public static int rank(int key, int[] a) {
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo <= hi) {
+			// Key is in a[lo..hi] or not present.
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid])
+				hi = mid - 1;
+			else if (key > a[mid])
+				lo = mid + 1;
+			else
+				return mid;
+		}
+		return -1;
+	}
 
-    public static void main(String[] args) {
-        int[] whitelist = In.readInts(args[0]);
+	public static void main(String[] args) {
+		int[] whitelist = In.readInts(args[0]);
 
-        Arrays.sort(whitelist);
+		Arrays.sort(whitelist);
 
-        // read key; print if not in whitelist
-        while (!StdIn.isEmpty()) {
-            int key = StdIn.readInt();
-            if (rank(key, whitelist) == -1)
-                StdOut.println(key);
-        }
-    }
+		// read key; print if not in whitelist
+		while (!StdIn.isEmpty()) {
+			int key = StdIn.readInt();
+			if (rank(key, whitelist) == -1)
+				StdOut.println(key);
+		}
+	}
 }
