90463f9404ab0f560ae40927ce8b5a06d50999d8
Robin Long <loveingenioustech@gmail.com>
2014-08-09 09:19:19 +0800

    0.1
    ReverseDemo
    Anagrams

---
+++ algorithm-demo/src/basic/ReverseDemo.java
@@ -0,0 +1,40 @@
+package basic;
+
+import stdlib.StdOut;
+
+public class ReverseDemo {
+
+	public static void main(String[] args) {
+		ReverseDemo demo = new ReverseDemo();
+
+		String originalText = "abcde";
+		StdOut.println("reverse: " + demo.reverse(originalText));
+		StdOut.println("inPlaceReverse: " + demo.inPlaceReverse(originalText));
+		StdOut.println("nativeReverse: " + demo.nativeReverse(originalText));
+
+	}
+
+	public String reverse(final String s) {
+		final StringBuilder builder = new StringBuilder(s.length());
+		for (int i = s.length() - 1; i >= 0; i--) {
+			builder.append(s.charAt(i));
+		}
+		return builder.toString();
+	}
+
+	public String inPlaceReverse(final String s) {
+		final StringBuilder builder = new StringBuilder(s);
+		for (int i = 0; i < builder.length() / 2; i++) {
+			final char tmp = builder.charAt(i);
+			final int otherEnd = builder.length() - i - 1;
+			builder.setCharAt(i, builder.charAt(otherEnd));
+			builder.setCharAt(otherEnd, tmp);
+		}
+		return builder.toString();
+	}
+
+	public String nativeReverse(final String s) {
+		return new StringBuilder(s).reverse().toString();
+	}
+
+}
